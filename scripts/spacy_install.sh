d1ba47ba72ff2d04c54d299902a0af4e17108263
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 22:01:08 -0400

    download spacy small default model

--- scripts/spacy_install.sh
+++ scripts/spacy_install.sh
@@ -2,5 +2,5 @@
 python -m pip install --upgrade pip
 pip install -U setuptools wheel
 pip install -U spacy
-python -m spacy download en_core_web_sb
+python -m spacy download en_core_web_sm
 exec "$@"
