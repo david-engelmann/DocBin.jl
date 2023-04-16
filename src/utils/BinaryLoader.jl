71f3d24d66e7c7170e130e151b9558162b88a373
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 00:13:48 -0400

    just read?

--- src/utils/BinaryLoader.jl
+++ src/utils/BinaryLoader.jl
@@ -2,5 +2,5 @@ function load_binary_from_file(file_path :: String)
     @info "location of the binary file:"
     @info file_path
     results = Array{Float64}
-    return read!(file_path, results)
+    return read(file_path)
 end
