17066d83a39fcf02dd8a894eafc08d8c21a26182
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 21:15:49 -0400

    add files for creating test spacy model & doc_bin

---
+++ scripts/spacy_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 spacy_model_and_doc_bin_creation.py
+exec "$@"
---
+++ scripts/spacy_model_and_doc_bin_creation.py
@@ -0,0 +1,11 @@
+import spacy
+from spacy.tokens import DocBin
+
+doc_bin = DocBin(store_user_data=False)
+nlp = spacy.load("en_core_web_sm")
+
+doc = nlp("Man, deviated leather in this 911\nBend a corner, I'm on Hayes Street copping a windbreaker")
+doc_bin.add(doc=doc)
+
+nlp.to_disk("test/assets/test_nlp")
+doc_bin.to_disk("test/assets/test_doc_bin.spacy")
