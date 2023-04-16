6c16d0aaf49c971ea7af1d25fe2aaa87fefd0205
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 00:19:28 -0400

    loading as Float64

--- src/utils/BinaryLoader.jl
+++ src/utils/BinaryLoader.jl
@@ -1,6 +1,5 @@
 function load_binary_from_file(file_path :: String)
     @info "location of the binary file:"
     @info file_path
-    results = Array{Float64}
-    return read(file_path)
+    return read(file_path, Float64)
 end
