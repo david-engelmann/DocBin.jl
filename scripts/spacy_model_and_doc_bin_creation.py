10206dbf24754325fd7a8ac3a33aa357e67ba322
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 22:08:26 -0400

    getcwd from model and doc bin creation

--- scripts/spacy_model_and_doc_bin_creation.py
+++ scripts/spacy_model_and_doc_bin_creation.py
@@ -1,4 +1,5 @@
 import spacy
+import os
 from spacy.tokens import DocBin
 
 doc_bin = DocBin(store_user_data=False)
@@ -6,6 +7,8 @@ nlp = spacy.load("en_core_web_sm")
 
 doc = nlp("Man, deviated leather in this 911\nBend a corner, I'm on Hayes Street copping a windbreaker")
 doc_bin.add(doc=doc)
+
+print(os.getcwd())
 
 nlp.to_disk("test/assets/test_nlp")
 doc_bin.to_disk("test/assets/test_doc_bin.spacy")
