07975b2c6291bc89050b298bb0dd58245769e4d6
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 00:33:36 -0400

    read first two float64s

--- src/utils/BinaryLoader.jl
+++ src/utils/BinaryLoader.jl
@@ -1,5 +1,12 @@
 function load_binary_from_file(file_path :: String)
     @info "location of the binary file:"
     @info file_path
-    return read(file_path, Float64)
+    n = read(file_path, Float64)
+    nt = read(file_path, Float64)
+    @info "n:"
+    @info n
+    @info "nt:"
+    @info nt
+    @test isa(n, Float64)
+    @test isa(nt, Float64)
 end
